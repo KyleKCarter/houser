633103b50f0712abdaf04112d4192377d15094c1
Kyle Carter <kkcarter1129@gmail.com>
2019-10-28 19:38:16 -0500

    Skills Check set up

---
+++ src/components/Dashboard/Dashboard.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react"
+
+//components
+import House from "../House/House";
+
+export default class Dashboard extends Component {
+    render() {
+        return (
+            <>
+                <div>Dashboard</div>
+                <House />
+            </>
+        )
+    }
+}
